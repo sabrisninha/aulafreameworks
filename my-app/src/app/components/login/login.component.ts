1f8400354a8c368722a8ce828358b32d88768983
sabrisninha <127432260+sabrisninha@users.noreply.github.com>
2023-04-06 14:53:28 -0400

    Add files via upload

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-isError= false;
+  isError = false;
+  isSucess = false;
+
+  user = ""
+  pwd = ""
+
+  login() {
+
+    console.log("user = " + this.user + "Password = " + this.pwd)
+    if (this.user == "psabrina260@gmail.com" && this.pwd == "sabrina28") {
+      console.log("login aprovado")
+      this.isError = false;
+      this.isSucess = true;
+    }
+    else {
+      this.isError = true;
+      this.isSucess = false;
+    }
+
+  }
 }
+
